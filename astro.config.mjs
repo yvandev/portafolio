46bd78d2bb430e5c597e0bd16ab72d26c3f4b430
Ivan Chirinos <chirinos.ivan@gmail.com>
2022-11-01 23:20:43 -0400

    Configuraciones.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [react(), image(), tailwind()]
+});
